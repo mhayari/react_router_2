005260511d1d0bf007e0cd2d8fc8a373b07f3da9
Your Name~ <you@example.com>
2022-12-01 03:37:13 +0100

    added

--- src/component/Home.js
+++ src/component/Home.js
@@ -6,18 +6,6 @@ import { NavLink } from "react-router-dom"
 const Home = ({filterMov,setFilterMov,handelFilterChange,films,handelWatchClick}) => {
   return (
     <>
-     {/* <div className="home">
-      <NavLink to='/'>Home</NavLink>
-      <NavLink to='add movies'>Add Movies</NavLink>
-      <NavLink to='about'>About</NavLink>
-      <NavLink to='contact'>Contact</NavLink>
-    </div> */}
-    {/* <div>
-    <input placeholder='Filter Title' type='search' name='title'  onChange={handelFilterChange} value={filterMov.title}  />
-            <input placeholder='Filter Rating' type='search' name='rating' onChange={handelFilterChange} value={filterMov.rating} />
-    </div> */}
-
-
       <MovieList  handelWatchClick={handelWatchClick} films={films.filter(film=>((film.title).toLowerCase()).includes((filterMov.title).toLowerCase())&&(film.rating>=Number(filterMov.rating)))}/>
       
       </>
